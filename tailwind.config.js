2bda7344f3825dff634c723756081227317093bf
taniasultana-dev <98141534+taniasultana-dev@users.noreply.github.com>
2025-01-26 12:02:27 +0600

    nijum-div-done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
+const { colors } = require('laravel-mix/src/Log');
+const { primary } = require('laravel-mix/src/Mix');
+
 module.exports = {
     content: [
         './train-schedule/**/*.html',
+        './tourist-spot/**/*.html'
     ],
     theme: {
-        extend: {}
+        extend: {
+            fontFamily: {
+                inter: ['Inter', 'serif'],
+            },
+            colors: {
+                primary:{
+                    dark:'#0E121B',
+                    default:'#525866',
+                    light:'#F2F5F8',
+                    
+                },
+            },
+        },
     },
     plugins: [
         // This plugin allows you to create custom Tailwind groups.
